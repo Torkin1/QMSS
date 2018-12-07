1db8cfcea5c4768d5804efc0da17d36111f31db5
Torkin1 <daniele.laprova2@gmail.com>
2018-12-07 16:28:57 +0100

    new file:   QMSS_module.py 	new file:   profiler.py

---
+++ QMSS_module.py
@@ -0,0 +1,127 @@
+"""
+file name: QMSS_module.py
+Authors: Mihai Jianu, Daniele La Prova, Lorenzo Mei
+Python version: 3.x
+QMSS_module contiene le definizioni di:
+quickSelectSort(l, select)
+recursiveQuickSelectSort(l, left, right, select)
+sampleMedianSelect(l, left, right, k)
+sampleMedian(l, offset)
+"""
+from selection.__init__ import printSwitch
+from random import randint
+from math import ceil
+from selection.Selection import partitionDet, recursiveQuickSelectRand, recursiveQuickSelectDet, trivialSelect, quickSelectRand
+
+def quickSelectSort(l, select):
+   """
+   @param l: list of integers
+   @param select: int, { 0 = sampleMedianSelect, 1 = quickSelectRand,
+                         2 = quickSelectDet }
+   @return None, side effect: calls recursiveQuickSelectSort
+   Esegue un quickSort sulla lista l chiamando recursiveQuickSelectSort,
+   partizionando attorno a un pivot estrattotramite l'algoritmo di selezione
+   indicato da select.
+   """
+   assert type(l) == list, "Error! Not a list"
+
+   recursiveQuickSelectSort(l, 0, len(l) - 1, select)
+
+
+def recursiveQuickSelectSort(l, left, right, select):
+   """
+   @param l: input list of integers
+   @param left: int
+   @param right: int
+   @param select: int
+   @return None, side effect: sorts list
+   Nucleo ricorsivo di quickSelectSort.
+   """
+   k = int((left + right) / 2) + 1
+
+   if left >= right:
+      return
+
+   elif select == 0:
+      pivot = sampleMedianSelect(l, left, right, k)
+
+   elif select == 1:
+      pivot = recursiveQuickSelectRand(l, left, right, k)
+
+   elif select == 2:
+      pivot = recursiveQuickSelectDet(l, left, right, k, 10, "QuickSelectDet")
+
+   #print(pivot)
+   #print("({},{})".format(left, right))
+
+   pIndex = partitionDet(l, left, right, pivot)
+
+   #print(l)
+
+   recursiveQuickSelectSort(l, left, pIndex - 1, select)
+
+   #print("({},{})".format(left, right))
+   #print(l)
+
+   recursiveQuickSelectSort(l, pIndex + 1, right, select)
+
+   #print("({},{})".format(left, right))
+   #print(l)
+
+
+def sampleMedianSelect(l, left, right, k):
+   """
+   @param l: list
+   @param left: int
+   @param right: int
+   @param k: int, elemento da estrarre
+   @return int
+   Estrae l'elemento k dalla lista l, partizionando attorno a un pivot calcolato
+   chiamando sampleMedian.
+   """
+   if left == right:
+      return l[left]
+
+   lenTuple = ceil(len(l) / 100)
+
+   if lenTuple == 1:
+      lenTuple += 1
+   #lenTuple = 500 if len(l) == 50k
+   vperno = sampleMedian(l[left : right + 1] , lenTuple) # m = ceil(int((right - left + 1)/ 5))
+
+   #print(f"vperno is {vperno}")
+
+   perno = partitionDet(l, left, right, vperno)
+
+   posperno = perno + 1
+   if posperno == k:
+      return l[perno]
+   elif posperno > k:
+      return sampleMedianSelect(l, left, perno - 1, k)
+   else:
+      return sampleMedianSelect(l, perno + 1, right, k)
+
+def sampleMedian (l, offset):
+   """
+   @param l: list
+   @param offset: int, grandezza delle tuple
+   @return int
+   Costruisce un sottinsieme V di l partizionando quest'ultima
+   in len(l) / offset tuple, estraendo un elemento a caso da ciascuna tupla e
+   inserendolo in V. Se len(V) > 5, ripete ricorsivamente le suddette operazioni,
+   altrimenti si calcola il mediano di V e lo restituisce.
+   """
+   if len(l) <= offset:
+
+      return quickSelectRand(l, int(len(l) / 2) + 1)
+
+   else:
+      temp = []
+   for i in range(0, len(l), offset):
+
+      oneTuple = [l[j] for j in range(i, i + offset) if j < len(l)]
+      # print(f"tuple is {oneTuple}")
+      temp.append(oneTuple[randint(0, len(oneTuple) - 1)])
+      # print (f"temp is {temp}")
+
+return sampleMedian(temp, offset)
---
+++ profiler.py
@@ -0,0 +1,123 @@
+"""
+File name: profiler.py
+Authors: Mihai Jianu, Daniele La Prova, Lorenzo Mei
+Python version: 3.x
+Script di profiling per gli algoritmi di ordinamento
+$ python3 -m profiler -h
+usage: python -m main [-h] [-m] [-r] [-d] [-o] size range
+profiles quickSelectionSort execution time with major sorting algorithms
+positional arguments:
+  size                 size of the list
+  range                maximum range of values generated
+optional arguments:
+  -h, --help           show this help message and exit
+  -m, --median         includes quickSelectSort with sampleMedianSelect
+  -r, --random         inlcudes quickSelectSort with quickSelectRand
+  -d, --deterministic  includes quickSelectSort with quickSelectDet
+  -o, --others         inlcudes major sorting algorithms
+"""
+from sys import argv
+import QMSS_module
+import argparse
+from cProfile import run
+import pstats
+from random import randint
+from sorting.Sorting import *
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser( "$ python3 -m profiler", epilog = "written by Mihai Jianu, Daniele La Prova, Lorenzo Mei", description = "profiles quickSelectionSort execution time with major sorting algorithms")
+    
+    parser.add_argument("-m", "--median", help = "includes quickSelectSort with sampleMedianSelect", action = "store_true")
+    parser.add_argument("-r", "--random", help = "inlcudes quickSelectSort with quickSelectRand", action = "store_true")
+    parser.add_argument("-d", "--deterministic", help = "includes quickSelectSort with quickSelectDet", action = "store_true")
+    parser.add_argument("-o", "--others", help = "inlcudes major sorting algorithms", action = "store_true")
+    parser.add_argument("size", type = int, help = "size of the list")
+    parser.add_argument("range", type = int, help = "maximum range of values generated")
+    
+    args = parser.parse_args()
+
+    l = [randint(0, args.range) for i in range(args.size)]
+    temp = l.copy()
+
+    #print(l)
+
+    if args.median:
+
+        run('QMSS_module.quickSelectSort(l, 0)', 'stats.txt')
+        pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
+        l = temp.copy()
+    
+    if args.random:
+
+        run('QMSS_module.quickSelectSort(l, 1)', 'stats.txt')
+        pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
+        l = temp.copy()
+
+    if args.deterministic:
+
+        run('QMSS_module.quickSelectSort(l, 2)', 'stats.txt')
+        pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
+        l = temp.copy()
+
+    #print(l)
+
+    if args.others:
+    
+        #Execution of selectionSort
+
+        run('selectionSort(l)', 'stats.txt')
+        pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
+        #Execution of insertionSort
+
+        l = temp.copy()
+
+        run('insertionSortDown(l)', 'stats.txt')
+        pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
+
+        #Execution of bubbleSort
+
+        l = temp.copy()
+
+        run('bubbleSort(l)', 'stats.txt')
+        pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
+
+        #Execution of mergeSort
+
+        l = temp.copy()
+
+        run('mergeSort(l)', 'stats.txt')
+        pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
+
+        #Execution of quickSort
+
+        l = temp.copy()
+
+        run('quickSort(l)', 'stats.txt')
+        pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
+
+        #Execution of heapSort
+
+        l = temp.copy()
+
+        run('heapSort(l)', 'stats.txt')
+        pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
+
+        #Execution of radixSort
+
+        l = temp.copy()
+
+        run('radixSort(l, 100, 10)', 'stats.txt')
+        pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
+
+        #Execution of Sort
+
+        l = temp.copy()
+
+        run('l.sort()', 'stats.txt')
+        pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
+
+    if not args.median and not args.random and not args.deterministic and not args.others:
+         print ("No algorithms specified, so  no actions were performed")
+         print ("use -h, --help flags for help")
+
