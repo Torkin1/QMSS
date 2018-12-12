bc3ffea7e5ba27474b2ddced526d4442b08ce8f2
mihaiJianu <45464636+mihaiJianu@users.noreply.github.com>
2018-12-12 21:02:31 +0100

    Add files via upload

--- profiler.py
+++ profiler.py
@@ -2,19 +2,13 @@
 File name: profiler.py
 Authors: Mihai Jianu, Daniele La Prova, Lorenzo Mei
 Python version: 3.x
-
 Script di profiling per gli algoritmi di ordinamento
-
 $ python3 -m profiler -h
-
 usage: python -m main [-h] [-m] [-r] [-d] [-o] size range
-
 profiles quickSelectionSort execution time with major sorting algorithms
-
 positional arguments:
   size                 size of the list
   range                maximum range of values generated
-
 optional arguments:
   -h, --help           show this help message and exit
   -m, --median         includes quickSelectSort with sampleMedianSelect
@@ -49,24 +43,32 @@ if __name__ == "__main__":
     parser.add_argument("size", type=int, help="size of the list")
     parser.add_argument("range", type=int, help="maximum range of values generated")
 
-    parser.add_argument("-ns", "--nearlysorted", type = int, help = "uses a list with first size / fraction elements sorted as input", nargs ="?", metavar = "percentage", const = 0, action = "store")
-    parser.add_argument("-re", "--reversed", help = "uses a reverse sorted list as input", action= "store_true")
+    parser.add_argument("-ns", "--nearlysorted", type=int,
+                        help="uses a list with first size / fraction elements sorted as input", nargs="?",
+                        metavar="percentage", const=0, action="store")
+    parser.add_argument("-re", "--reversed", help="uses a reverse sorted list as input", action="store_true")
 
     args = parser.parse_args()
 
-    l = [randint(0, args.range) for i in range(args.size)]
-    
+
     if args.nearlysorted:
-       # print (args.nearlysorted)
-       # print (l)
-        b = l[0:int(args.size * (args.nearlysorted/100))]
+        #print(args.range)
+        #print(args.nearlysorted)
+        #print(int(args.range * (args.nearlysorted / 100)-1))
+        l = [randint(0,int(args.range * (args.nearlysorted / 100)-1)) for i in range(ceil(args.size * (args.nearlysorted / 100)))]
+        l2 = [randint(int(args.range * (args.nearlysorted / 100))-1, args.range) for i in range(args.size -ceil(args.size * (args.nearlysorted / 100)))]
+        l = l + l2
+        #print (args.nearlysorted)
+        #print (l)
+        b = l[0:ceil(args.size * (args.nearlysorted / 100))]
         b.sort()
-        l = b + l[int(args.size * (args.nearlysorted/100)):]
-      #  print (l)
-    
-    elif args.reversed:
-        l.sort(reverse = True)
-    
+        l = b + l[int(args.size * (args.nearlysorted / 100)):]
+        #print (l)
+    else:
+        l = [randint(0, args.range) for i in range(args.size)]
+        if args.reversed:
+            l.sort(reverse=True)
+
     temp = l.copy()
 
     # print(l)
@@ -145,4 +147,3 @@ if __name__ == "__main__":
     if not args.median and not args.random and not args.deterministic and not args.others:
         print("No algorithms specified, so  no actions were performed")
         print("use -h, --help flags for help")
-
